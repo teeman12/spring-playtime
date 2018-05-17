54c5dbd08c37111860f575cb77ae45fcd454e96a
Nameet Bansal <bansal@deshaw.com>
2018-05-17 11:39:24 +0530

    Changes for adding basic spring boot

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # spring-playtime
 general code for playing with features of spring
+
+
+#Starting the application
+./gradlew build && java -jar -Dserver.port=8679 build/libs/spring-playtime-0.1.0.jar
---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,33 @@
+package hello;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String args[]){
+        SpringApplication.run(Application.class,args );
+
+
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println(" INspect the beans provided by Spring boot !!");
+
+            String[] beans = ctx.getBeanDefinitionNames();
+            Arrays.sort(beans);
+
+            for(String bean : beans){
+                System.out.println(bean);
+            }
+        };
+
+    }
+}
